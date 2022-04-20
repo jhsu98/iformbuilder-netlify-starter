2e3ca768e01ae7eea0dbbb5b5a6d0d704c7968ee
jhsu98 <jhsu98@gmail.com>
2022-04-20 11:26:25 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/56880fbf-5557-47d8-8996-35ec7f7de7e1/deploy-status)](https://app.netlify.com/sites/reliable-pie-14777b/deploys)
 # iformbuilder-netlify-starter
 Starter repo for deploying iFormBuilder 
+
+## Getting Started
+
--- functions/hello-ifb.js
+++ functions/hello-ifb.js
@@ -1,21 +1,29 @@
 const zerionAPI = require('zerion-js');
 
 exports.handler = async (event) => {
-    const SERVER = process.env.SERVER;
-    const CLIENT_KEY = process.env.CLIENT_KEY;
-    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+    const IFB_SERVER = process.env.IFB_SERVER;
+    const IFB_CLIENT_KEY = process.env.IFB_CLIENT_KEY;
+    const IFB_CLIENT_SECRET = process.env.IFB_CLIENT_SECRET;
     const PARAMS = {
-        ifb_region: 'US'
+        ifb_region: 'uk',
+        
     };
+    try {
+        const ifb = new zerionAPI.IFB(IFB_SERVER, IFB_CLIENT_KEY, IFB_CLIENT_SECRET, PARAMS);
+        await ifb.connect();
 
-    const ifb = new zerionAPI.IFB(SERVER, CLIENT_KEY, CLIENT_SECRET, PARAMS);
-    await ifb.connect();
-
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({access_token: ifb.access_token})
-    };
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({access_token: ifb.access_token})
+        };
+    } catch(err) {
+        return {
+            statusCode: 500,
+            body: err.message || err
+        }
+    }
+    
 }
